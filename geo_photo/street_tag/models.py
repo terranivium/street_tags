af4a726ecfb9f91d753053d4f174c2dff2d9c0eb
terranivium <wesleyscottuk@gmail.com>
2020-10-31 13:19:57 +0000

    implemented adding markers and images via SQL DB

--- geo_photo/street_tag/models.py
+++ geo_photo/street_tag/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+	lon = models.DecimalField(max_digits=9, decimal_places=6)
+	lat = models.DecimalField(max_digits=9, decimal_places=6)
+	image = models.ImageField(blank=True)
--- geo_photo/street_tag/views.py
+++ geo_photo/street_tag/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from street_tag.models import Photo
 from django.http import HttpResponse 
 
 def index(request):
 	mapbox_access_token = 'pk.eyJ1IjoidGVycmFuaXZpdW0iLCJhIjoiY2tndGwxMmk3MGY1bTJ5bmFud3l4d2d2eSJ9.YaCL14cu5Hfc1dIXZcTN_A'
-	return render(request, 'street_tag/index.html', { 'mapbox_access_token':mapbox_access_token })
+	photo_list = Photo.objects.order_by()[:5] #order by?
+
+	context_dict = {}
+	context_dict['photos'] = photo_list
+	context_dict['mapbox_access_token'] = mapbox_access_token
+
+	return render(request, 'street_tag/index.html', context=context_dict)
