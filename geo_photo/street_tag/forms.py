6fdbc6b39689037437e0be8381f6cb699c84368b
terranivium <wesleyscottuk@gmail.com>
2020-11-01 16:04:55 +0000

    fixed add photo form error checking, all basic functionality implemented, shows add_photo on index

--- geo_photo/street_tag/forms.py
+++ geo_photo/street_tag/forms.py
@@ -4,7 +4,7 @@ from street_tag.models import Photo
 class PhotoForm(forms.ModelForm):
 	lon = forms.DecimalField(max_digits=9, decimal_places=6, help_text="Please enter the lon.")
 	lat = forms.DecimalField(max_digits=9, decimal_places=6, help_text="Please enter the lat.")
-	image = forms.ImageField(help_text="Please enter image.", required = False)
+	image = forms.ImageField(help_text="Please enter image.", required = True)
 
 	# An inline class to provide
 	class Meta:
---
+++ readme.md
@@ -0,0 +1,20 @@
+### Street Tags
+## Street Photography Location Tagging WebAPP
+
+Technologies Used:
+
+Anaconda (Virtual Environment)
+Django (Web Framework)
+Python
+SQLite (DB)
+JavaScript
+Mapbox API
+
+For an online demo see:
+
+How to use this repo:
+
+
+
+
+
