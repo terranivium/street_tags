5c16ade4cc1ebb815be700834a8e47d870e1534c
terranivium <wesleyscottuk@gmail.com>
2020-11-02 12:15:17 +0000

    implemented basic image validation

--- geo_photo/street_tag/models.py
+++ geo_photo/street_tag/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+# image upload validation
+def validate_image(image):
+	max_height = 350
+	max_width = 350
+	height = image.height
+	width = image.width
+	if width > max_width or height > max_height:
+		raise ValidationError("Height or Width is larger than what is allowed (350px)")
 
 class Photo(models.Model):
 	lon = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
 	lat = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
-	image = models.ImageField(blank=True, null=True)
+	image = models.ImageField(blank=True, null=True, validators=[validate_image])
