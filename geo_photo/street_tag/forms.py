2e16bdb42f2a04607eabc205741f00a313588d19
terranivium <wesleyscottuk@gmail.com>
2020-11-02 17:02:30 +0000

    added bootstrap template, included requirements.txt

--- geo_photo/street_tag/forms.py
+++ geo_photo/street_tag/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from street_tag.models import Photo
 
 class PhotoForm(forms.ModelForm):
-	lon = forms.DecimalField(max_digits=9, decimal_places=6, help_text="Please enter the lon.")
-	lat = forms.DecimalField(max_digits=9, decimal_places=6, help_text="Please enter the lat.")
+	lon = forms.DecimalField(max_digits=9, decimal_places=6, help_text="Please enter the longitude.")
+	lat = forms.DecimalField(max_digits=9, decimal_places=6, help_text="Please enter the latitude.")
 	image = forms.ImageField(help_text="Please enter image.", required = True)
 
 	# An inline class to provide
